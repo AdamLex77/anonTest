d8a94e915a6eeed4df6a1e9ee8ea03ef2f7b0ee4
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2023-06-07 04:02:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -498,7 +498,7 @@ class ChatBot:
 
 
 if __name__ == '__main__':
-    bot_name = "One Night Stand"
+    bot_name = "cintasatumalambot"
     bot_key = "5921989048:AAFSW9El44N5lcbVbBvhHeCeSM9eQsHqpNM"
 
     ChatBot(bot_name, bot_key)
