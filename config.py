b1406ef7357061bc93e79fed739a9e610f7ceee5
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2023-06-07 14:53:59 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 api_id = int(os.environ.get("API_ID", "20244703"))
 api_hash = os.environ.get("API_HASH", "aecd498f68e85c1c925a6a9f95103aee")
-bot_token = os.environ.get("BOT_TOKEN", "6252348567:AAH86gAM0vKKYZppyCWu-tMUnclCdoUqKA4")
+bot_token = os.environ.get("BOT_TOKEN", "6252348567:AAGH1trfi-TmGL3mtBgEyeTaqJgRv7_RGPo")
 
 db_url = os.environ.get("DB_URL", "mongodb+srv://kuloit:susuboneto123@cluster0.kbtznjt.mongodb.net/?retryWrites=true&w=majority")
