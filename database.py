c6298f8878d34a1456026312799684c4416e41bc
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2023-06-07 12:26:47 +0700

    Update database.py

--- database.py
+++ database.py
@@ -28,15 +28,23 @@ class DataBase:
 
         return result
 
-    def insert(self, user_id, name, username):
+    def insert(self, user_id, name, username, age, tempat):
         # time-stamp
-        IST = pytz.timezone('Asia/Kolkata')
+        IST = pytz.timezone('Asia/Jakarta')
         time_stamp = datetime.now(IST).strftime('%Y-%m-%d || %H:%M:%S')
 
         self.dataset.insert_one({"_id": user_id,
                                  "name": name,
                                  "username": username,
-                                 "timestamp": time_stamp})
+                                 "timestamp": time_stamp,
+                                 "umur": age,
+                                 "domisili": tempat})
 
     def update(self, user_id, new_data):
         self.dataset.update_one({"_id": user_id}, {"$set": new_data})
+
+    def umur(self, age, new_data):
+        self.dataset.update_one({"umur": age}, {"$set": new_data})
+    
+    def dom(self, tempat, new_data):
+        self.dataset.update_one({"domisili": tempat}, {"$set": new_data})
