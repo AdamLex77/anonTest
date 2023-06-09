0caedfe7359aad8848edd79603bf491b351d5fe8
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2023-06-10 05:59:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ class ChatBot:
                 context.bot.send_chat_action(chat_id=user_id, action=ChatAction.TYPING, timeout=1)
 
                 context.bot.send_message(chat_id=user_id, text=age_user())
-                dp.add_handler(self.domisili_handler, run_async=True)
+                dp.add_handler(self.domisili_handler)
             # if user stop the bot
             except telegram.error.Unauthorized:
                 pass
@@ -128,7 +128,7 @@ class ChatBot:
                 context.bot.send_message(chat_id=user_id, text=domisili_user())
                 new_data = {"old": {text}}
                 self.record.update(user_id, new_data)
-                dp.add_handler(slef.gender_handler, run_async=True)
+                dp.add_handler(self.gender_handler)
             # if user stop the bot
             except telegram.error.Unauthorized:
                 pass
