4305a31fe1a59eef1199b54a1cc473ebbde1a70c
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2023-06-10 04:55:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ class ChatBot:
                 # Typing Action
                 context.bot.send_chat_action(chat_id=user_id, action=ChatAction.TYPING, timeout=1)
 
-                context.bot.send_message(chat_id=user_id, text=name_user())
+                context.bot.send_message(chat_id=user_id, text=age_user())
                 self.domisili_handler(update, context)
             # if user stop the bot
             except telegram.error.Unauthorized:
@@ -115,6 +115,7 @@ class ChatBot:
                 if not text.isdigit():
                     context.bot.send_message(chat_id=user_id, text=wrong_name(), parse_mode="markdown")
                     self.settings(update, context)
+                    return
                 # Typing Action
                 context.bot.send_chat_action(chat_id=user_id, action=ChatAction.TYPING, timeout=1)
 
