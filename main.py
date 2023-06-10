90fc627bab23711c1c8fae9b7fb559b3ba4553f3
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2023-06-10 19:18:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -153,21 +153,21 @@ class ChatBot:
         gender_list.remove(user_id)
         opp_gender_list.remove(partner)
 
-        data = self.record.search(user_id)
-
         # updating chat pairs
         self.chat_pair.update({user_id: partner})
         self.chat_pair.update({partner: user_id})
 
         if gender1:
+            data = self.record.search(user_id)
             my_old = data.get("old")
             my_dom = data.get("domisili")
             my_name = data.get("name")
 
         if gender2:
-            my_olddi = data.get("old")
-            my_dommi = data.get("domisili")
-            my_nameei = data.get("name")
+            diti = self.record.search(partner)
+            my_olddi = diti.get("old")
+            my_dommi = diti.get("domisili")
+            my_nameei = diti.get("name")
 
         if gender1 == "Boy":
             pirtin = "🤴🏻 Boy"
@@ -181,7 +181,7 @@ class ChatBot:
         if gender1:
             context.bot.send_message(chat_id=user_id, text=f"*find your partner*\n\nname: {my_nameei}\nage: {my_olddi}\ntempat tinggal: {my_dommi}\ngender:{pirtin}\n\n/next — find a new partner\n/stop — stop this dialog", parse_mode='Markdown')
         if gender2:
-            context.bot.send_message(chat_id=user_id, text=f"*find your partner*\n\nname: {my_name}\nage: {my_old}\ntempat tinggal: {my_dom}\ngender:{pirtinin}\n\n/next — find a new partner\n/stop — stop this dialog", parse_mode='Markdown')
+            context.bot.send_message(chat_id=partner, text=f"*find your partner*\n\nname: {my_name}\nage: {my_old}\ntempat tinggal: {my_dom}\ngender:{pirtinin}\n\n/next — find a new partner\n/stop — stop this dialog", parse_mode='Markdown')
 
     def find_partner(self, update, context):
         user_id, name, username = self.common_args(update, context)
