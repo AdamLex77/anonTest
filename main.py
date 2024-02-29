557c49b52d844a208179895a7047a2520790eb5b
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-03-01 01:54:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import urllib3
 CHANNELS = ["@onsbase"]
 owner_id = -1001774215660
 
-async def age_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def age_command(update, context):
     await update.message.reply_text('how old are you?')
 
 
